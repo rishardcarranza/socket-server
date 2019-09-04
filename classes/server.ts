48cc077826d73a555a9c0664b2d24db3fe880ca4
Richard Carranza <rishardcarranza@gmail.com>
2019-09-03 22:02:50 -0600

    4.6 Estado del servidor

--- classes/server.ts
+++ classes/server.ts
@@ -1,16 +1,40 @@
 import express from 'express';
 import { SERVER_PORT } from '../global/environment';
+import socketIO from 'socket.io';
+import http from 'http';
 
 export default class Server {
+    private static _instance: Server;
+
     public app: express.Application;
     public port: number;
 
-    constructor() {
+    public io: SocketIO.Server;
+    private httpServer: http.Server;
+
+    private constructor() {
         this.app = express();
         this.port = SERVER_PORT
+
+        this.httpServer = new http.Server(this.app);
+        this.io = socketIO(this.httpServer);
+
+        this.listenSockets();
+    }
+
+    public static get instance() {
+        return this._instance || (this._instance = new this());
+    }
+
+    private listenSockets() {
+        console.log('Escuchando - sockets');
+
+        this.io.on('connection', cliente => {
+            console.log('Cliente conectado');
+        });
     }
 
     start(callback: Function) {
-        this.app.listen(this.port, callback());
+        this.httpServer.listen(this.port, callback());
     }
 }
